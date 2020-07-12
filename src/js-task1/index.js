c40a8c9dc30ffa60c30da5f6dc214f15adb2eb04
Maksim Kashytski <makgoo@ya.ru>
2020-07-12 17:24:21 +0300

    hot fix

--- src/js-task1/index.js
+++ src/js-task1/index.js
@@ -36,10 +36,12 @@ function sqrN(x, n) {
 
 function factorialLoop(x) {
   let res = 1;
+  let number = x;
 
   do {
-    res *= x--;
-  } while (x > 0)
+    res *= number;
+    number -= 1;
+  } while (number > 0);
 
   return res;
 }
@@ -50,19 +52,21 @@ function factorialRecursion(x) {
 
 function round(x) {
   const sign = x < 0 ? -1 : 1;
+  let number = x;
 
-  x *= sign;
-  x = x < ~~ x + 0.5 ? x | 0 : x | 1;
+  number *= sign;
+  number = number < ~~number + 0.5 ? number | 0 : ~~number + 1;
 
-  return x * sign;
+  return number * sign;
 }
 
 function rectangle(width, height) {
   const base = '*\xa0'.repeat(width);
   let rect = base;
 
-  for (let i = 0; i < height - 1; i++) {
-    rect += '\n' + base; 
+  for (let i = 0; i < height - 1; i += 1) {
+    rect += '\n';
+    rect += base;
   }
 
   return rect;
@@ -72,8 +76,9 @@ function rightTriangle(lines) {
   const vertex = '*\xa0';
   let triangle = vertex;
 
-  for (let i = 2; i <= lines; i++) {
-    triangle += '\n' + vertex.repeat(i);
+  for (let i = 2; i <= lines; i += 1) {
+    triangle += '\n';
+    triangle += vertex.repeat(i);
   }
 
   return triangle;
@@ -82,10 +87,16 @@ function rightTriangle(lines) {
 function equilateralTriangle(lines) {
   let base = '*'.repeat(lines * 2 - 1);
   let triangle = base;
+  let step = '';
 
-  for (i = 1; i < lines; i++) {
+  for (let i = 1; i < lines; i += 1) {
     base = base.substring(1, base.length - 1);
-    triangle = '\xa0'.repeat(i) + base + '\xa0'.repeat(i) + '\n' + triangle;
+    step = '\xa0'.repeat(i);
+    step += base;
+    step += '\xa0'.repeat(i);
+    step += '\n';
+    step += triangle;
+    triangle = step;
   }
 
   return triangle;
@@ -93,19 +104,50 @@ function equilateralTriangle(lines) {
 
 function rhombus(lines) {
   let base = '*'.repeat(lines * 2 + 1);
-  let rhombus = base;
+  let rhomb = base;
+  let step = '';
 
-  for (i = 1; i < lines + 1; i++) {
+  for (let i = 1; i < lines + 1; i += 1) {
     base = base.substring(1, base.length - 1);
-    rhombus = '\xa0'.repeat(i) + base + '\xa0'.repeat(i) + '\n' + rhombus;
+    step = '\xa0'.repeat(i);
+    step += base;
+    step += '\xa0'.repeat(i);
+    step += '\n';
+    step += rhomb;
+    rhomb = step;
   }
 
   base = '*'.repeat(lines * 2 + 1);
 
-  for (i = 1; i < lines + 1; i++) {
+  for (let i = 1; i < lines + 1; i += 1) {
     base = base.substring(1, base.length - 1);
-    rhombus += '\n' + '\xa0'.repeat(i) + base + '\xa0'.repeat(i);
+    rhomb += '\n';
+    rhomb += '\xa0'.repeat(i);
+    rhomb += base;
+    rhomb += '\xa0'.repeat(i);
   }
 
-  return rhombus;
+  return rhomb;
 }
+
+console.log(`sum(1, 2) = ${sum(1, 2)}`);
+console.log(`sub(2, 1) = ${sub(2, 1)}`);
+console.log(`multiply(5, 3) = ${multiply(5, 3)}`);
+console.log(`percentage(10, 5) = ${percentage(10, 5)}`);
+console.log(`div(1, 2) = ${div(1, 2)}`);
+console.log(`integerDiv(9, 2) = ${integerDiv(9, 2)}`);
+console.log(`abs(-1) = ${abs(-1)}`);
+console.log(`sqr(5) = ${sqr(5)}`);
+console.log(`sqrN(2, 4) = ${sqrN(2, 4)}`);
+console.log(`sqrN(2, 0) = ${sqrN(2, 0)}`);
+console.log(`sqrN(2, -1) = ${sqrN(2, -1)}`);
+console.log(`factorialLoop(4) = ${factorialLoop(4)}`);
+console.log(`factorialRecursion(4) = ${factorialRecursion(4)}`);
+console.log(`round(5.8) = ${round(5.8)}`);
+console.log(`round(5.2) = ${round(5.2)}`);
+console.log(`round(-2.8) = ${round(-2.8)}`);
+console.log(`round(-2.2) = ${round(-2.2)}`);
+console.log(rectangle(4, 5));
+console.log(rightTriangle(5));
+console.log(equilateralTriangle(5));
+console.log(rhombus(4));
